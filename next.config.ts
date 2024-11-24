c6a80786ffb445012c755de570bd3d2f403283ed
willswats <william@williamwatson.dev>
2024-11-24 15:43:34 +0000

    refactor: set next.config.ts back to default

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
-
-    return config;
-  },
-
-  experimental: {
-    turbo: {
-      rules: {
-        "*.svg": {
-          loaders: ["@svgr/webpack"],
-          as: "*.js",
-        },
-      },
-    },
-  },
+  /* config options here */
 };
+
 export default nextConfig;
